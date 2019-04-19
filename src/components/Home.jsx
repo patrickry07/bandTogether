e78156349f9ee1c833ff645bc17d74e882753027
Patrick Ryan <patrickry07@gmail.com>
2019-04-19 17:30:00 -0500

    fixed merge

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ import Map from './Map.jsx'
 
 const popover = (
   <Popover id="popover-basic">
-      Must be logged in to create a listing!
+    Must be logged in to create a listing!
   </Popover>
 )
 class Home extends React.Component {
@@ -29,7 +29,7 @@ class Home extends React.Component {
   }
 
   componentDidUpdate(prevProp, prevState, snapshot) {
-    if(prevProp.listings !== this.props.listings) {
+    if (prevProp.listings !== this.props.listings) {
       this.setSort(this.state.sort);
     }
   }
@@ -47,14 +47,14 @@ class Home extends React.Component {
       return listing;
     })
     let sortProperty, listings;
-    if(sort === 'date') {
+    if (sort === 'date') {
       listings = artistAddedListings.sort((a, b) => {
         // the date is a string like so: '<year>-<month>-<day>'.
         // this function splits the date and checks to see what events will come first in order to return a sort value.
         const aArray = a.date.split('-');
         const bArray = b.date.split('-');
-        if(aArray[0] === bArray[0]) {
-          if(aArray[1] === bArray[1]) {
+        if (aArray[0] === bArray[0]) {
+          if (aArray[1] === bArray[1]) {
             if (aArray[2] > bArray[2]) {
               return 1;
             }
@@ -67,13 +67,13 @@ class Home extends React.Component {
         return -1;
       });
     } else {
-      if(sort === 'event name') {
+      if (sort === 'event name') {
         sortProperty = 'title';
       }
-      if(sort === 'band name') {
+      if (sort === 'band name') {
         sortProperty = 'artistName';
       }
-      if(sort === 'genre') {
+      if (sort === 'genre') {
         sortProperty = 'genre';
       }
       listings = artistAddedListings.sort((a, b) => a[sortProperty].toUpperCase() > b[sortProperty].toUpperCase() ? 1 : -1)
@@ -110,29 +110,38 @@ class Home extends React.Component {
     return (
       <div className="jumbotron">
         <div className='row justify-content-center'>
-          <img src='https://i.imgur.com/VvaYR3a.png' alt="Band Together"/>
+          <img src='https://i.imgur.com/VvaYR3a.png' alt="Band Together" />
         </div>
-        <div className="row">
-          <div className="col-md-12" style={{marginBottom: "20px"}}>
+
+        <div className="row" id="menu-map">
+
+
+          <div className="col-md-5" style={{ marginBottom: "20px" }}>
             {!showSearch && <button className="btn btn-dark btn-lg btn-block" type="button" onClick={this.toggleSearch}>Search Settings</button>}
-            {showSearch && <Search toggleSearch={this.toggleSearch} filters={filters} searchCityValue={searchCityValue} setSearchCityValue={setSearchCityValue} setFilters={this.props.setFilters} setSort={this.setSort} sort={sort}/>}
+            {showSearch && <Search toggleSearch={this.toggleSearch} filters={filters} searchCityValue={searchCityValue} setSearchCityValue={setSearchCityValue} setFilters={this.props.setFilters} setSort={this.setSort} sort={sort} />}
             {!isLoggedIn && (
               <OverlayTrigger trigger="click" placement="top" overlay={popover}>
                 <button className="btn btn-dark btn-lg btn-block" type="button">Create a Listing</button>
               </OverlayTrigger>
             )}
-            {isLoggedIn && 
+            {isLoggedIn &&
               ((!showForm && <button className="btn btn-dark btn-lg btn-block" type="button" onClick={this.toggleForm}>Create a Listing</button>)
-              ||
-              (showForm && <ListingForm handleNewListing={handleNewListing} toggleForm={this.toggleForm} />))
+                ||
+                (showForm && <ListingForm handleNewListing={handleNewListing} toggleForm={this.toggleForm} />))
             }
           </div>
-        </div>
+
+
+          <div className="col-md-7 justify-content-center">
             <div id="map-div">
-            <Map listings={listings} />
+              <Map listings={listings} />
             </div>
+          </div>
+
+        </div>
+
         <div className="row">
-          <ListView changeProfile={changeProfile} isLoggedIn={isLoggedIn} listings={listings} artists={artists} userProfile={userProfile}/>
+          <ListView changeProfile={changeProfile} isLoggedIn={isLoggedIn} listings={listings} artists={artists} userProfile={userProfile} />
         </div>
       </div>
     )
